518779936b60143217debc24615125e26e8da1ba
unknown <gavrisnazar981@gmail.com>
2024-06-08 12:56:11 +0300

    second lesson

--- second_lesson/blog/urls.py
+++ second_lesson/blog/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from blog import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('/product/<int:product_id>', views.product, name='product')
 ]
--- second_lesson/blog/views.py
+++ second_lesson/blog/views.py
@@ -1,11 +1,39 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+items = {
+    'Smartphone': {
+        'id': 1,
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    },
+    'Laptop': {
+        'id': 2,
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    },
+    'Keyboard': {
+        'id': 3,
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    },
+    'Mouse': {
+        'id': 4,
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+    }
+}
+
 
 def home(request):
     menu = ['home', 'about', 'posts',]
-    data = {'menu': menu}
+    data = {'menu': menu,
+            'items': items}
     return render(request, 'blog/index.html', context=data)
+
+
+def product(request, product_id):
+    product_lst = [i for i in items if items[i]['id'] == product_id]
+
+    return render(request, 'blog/product.html',
+                  {'product_desc': items[product_lst[0]], 'product_name': product_lst[0]})
+
 
 def about(request):
     return render(request, 'blog/about.html')
